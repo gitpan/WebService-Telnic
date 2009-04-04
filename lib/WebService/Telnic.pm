14e63859cb2157c7f892910d35445102cbed70f1
Peter Makholm <pmakholm@cpan.org>
2009-04-04 17:54:01 +0000

    Import of PMAKHOLM/WebService-Telnic-0.1 from CPAN.
    gitpan-cpan-distribution: WebService-Telnic
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         PMAKHOLM/WebService-Telnic-0.1.tar.gz
    gitpan-cpan-author:       PMAKHOLM
    gitpan-cpan-maturity:     released

---
+++ lib/WebService/Telnic.pm
@@ -0,0 +1,113 @@
+package WebService::Telnic;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+WebService::Telnic - Interface to Telnic's SOAP API's
+
+=head1 VERSION
+
+Version 0.1
+
+=cut
+
+our $VERSION = '0.1';
+
+=head1 AUTHOR
+
+Peter Makholm, C<< <peter at makholm.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-webservice-telnic at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WebService-Telnic>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SEE ALSO
+
+L<WebService::Telnic::Client>
+
+
+=head1 SUPPORT
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WebService-Telnic>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WebService-Telnic>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WebService-Telnic>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WebService-Telnic>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Peter Makholm, all rights reserved.
+
+This software is released under the MIT license cited below.
+
+=head1 The "MIT" License
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject
+to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+=cut
+
+1; # End of WebService::Telnic
---
+++ lib/WebService/Telnic/Client.pm
@@ -0,0 +1,135 @@
+package WebService::Telnic::Client;
+
+use warnings;
+use strict;
+
+use XML::Simple;
+use Net::DNS::RR;
+
+use base qw(WebService::Telnic::Base WebService::Telnic::Client::Record WebService::Telnic::Client::Profile);
+
+our $VERSION = '0.1';
+
+sub new {
+    my $class = shift;
+    my $self  = $class->SUPER::new(@_);
+
+    $self->{namespaces} = {
+	%{ $self->{namespaces} },
+	domain     => 'http://xmlns.telnic.org/ws/nsp/client/domain/types-1.0',
+	record     => 'http://xmlns.telnic.org/ws/nsp/client/record/types-1.0',
+	reader     => 'http://xmlns.telnic.org/ws/nsp/client/reader/types-1.0',
+	profile    => 'http://xmlns.telnic.org/ws/nsp/client/profile/types-1.0',
+	searchdata => 'http://xmlns.telnic.org/ws/nsp/client/searchdata/types-1.0',
+	exchange   => 'http://xmlns.telnic.org/ws/nsp/client/exchange/types-1.0',
+    };
+
+    return $self;
+}
+
+=head1 NAME
+
+WebService::Telnic::Client - Interface to the Telnic Client API
+
+=head1 VERSION
+
+Version 0.1
+
+=head1 SYNOPSIS
+
+    use WebService::Telnic::Client;
+
+    my $client = WebService::Telnic::Client->new(
+        endpoint => 'https://telprovider.com/client',
+        user     => 'exampletel',
+        pass     => 'XXXXXXXX,
+    )
+
+    my $records  = $client->listRecords('example.tel');
+    my $profiles = $client->listProfilesExt('example.net');
+
+At the moment only handling of resource records and profiles is implemented.
+All methods takes the domain name as first argument.
+
+=head1 Record
+
+=head2 listRecords
+
+=head2 storeRecord
+
+=head2 updateRecords
+
+=head2 deleteRecord
+
+=head Profile
+
+=head2 createProfile
+
+=head2 deleteProfile
+
+=head2 updateProfile
+
+=head2 listProfiles
+
+=head2 listProfilesExt
+
+=head2 getProfile
+
+=head2 switchToProfile
+
+=head2 getActiveProfile
+
+=head1 AUTHOR
+
+Peter Makholm, C<< <peter at makholm.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-webservice-telnic-client at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WebService-Telnic>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can look for information at:
+
+=over 4
+
+=item * github: Public version control system
+
+L<http://github.com/pmakholm/telnic-perl/tree>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WebService-Telnic>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WebService-Telnic>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WebService-Telnic>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WebService-Telnic>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Peter Makholm, all rights reserved.
+
+This software is released under the MIT license cited in L<WebService::Telnic>.
+
+=cut
+
+1; # End of WebService::Telnic::Client
